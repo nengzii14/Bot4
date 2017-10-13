b21d4c706934c5f1849bfdcf4e92230280c5a1a0
Bama Septituta <32743166+initialaf@users.noreply.github.com>
2017-10-13 09:44:06 -0700

    Update vbot.py

--- vbot.py
+++ vbot.py
@@ -296,27 +296,28 @@ def SEND_MESSAGE(op):
                     sendMessage(msg.to, text=None, contentMetadata={'mid': key}, contentType=13)
                     contact = client.getContact(key)
                     sendMessage(msg.to, "Kontaknya si "+contact.displayName+"")
-                if msg.text in ["tagall"]:
-                    group = client.getGroup(msg.to)
-               	    nama = [contact.mid for contact in group.members]
-                    cb = ""
-                    cb2 = ""
-                    strt = int(0)
-                    akh = int(0)
-                    for md in nama:
-                    	akh = akh + int(5)
-                    	cb += """{"S":"""+json.dumps(str(strt))+""","E":"""+json.dumps(str(akh))+""","M":"""+json.dumps(md)+"},"""
-                    	strt = strt + int(6)
-                    	akh = akh + 1
-                    	cb2 += "@nrik\n"
-                	cb = (cb[:int(len(cb)-1)])
-                	msg.contentType = 0
-                	msg.text = cb2
-                	msg.contentMetadata ={'MENTION':'{"MENTIONEES":['+cb+']}','EMTVER':'4'}
-                	try:
-                    	    client.sendMessage(msg)
-                	except Exception as error:
-                            print error
+                if msg.text == "tagall":
+		    group = client.getGroup(msg.to)
+		    nama = [contact.mid for contact in group.members]
+		    cb = ""
+		    cb2 = ""
+		    strt = int(0)
+		    akh = int(0)
+		    for md in nama:
+			akh = akh + int(5)	
+			cb += """{"S":"""+json.dumps(str(strt))+""","E":"""+json.dumps(str(akh))+""","M":"""+json.dumps(md)+"},"""
+			strt = strt + int(6)
+			akh = akh + 1
+			cb2 += "@nrik\n"
+		   
+		    cb = (cb[:int(len(cb)-1)])
+		    msg.contentType = 0
+		    msg.text = cb2
+		    msg.contentMetadata ={'MENTION':'{"MENTIONEES":['+cb+']}','EMTVER':'4'}
+		    try:
+		        client.sendMessage(msg)
+		    except Exception as error:
+			print error	
                 if msg.text == "time":
                     sendMessage(msg.to, "Tanggal sekarang = " + datetime.datetime.today().strftime('%d-%m-%y'))
 		    sendMessage(msg.to, "Waktu sekarang = " + datetime.datetime.today().strftime('%H:%M:%S'))
